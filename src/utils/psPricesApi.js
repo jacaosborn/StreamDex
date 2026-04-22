7f42b4098431619386a49c064dcfbd3d07b514ac
Jacob <jacaosborn@gmail.com>
2026-04-22 17:39:27 -0500

    security update

--- src/utils/psPricesApi.js
+++ src/utils/psPricesApi.js
@@ -1,4 +1,3 @@
-const apiKey = "81240253-d2fb-4ede-812d-30d0838d5391";
 const baseUrl =
   import.meta.env.VITE_API_URL ||
   "https://streamdexbackend.onrender.com/api/games?";
@@ -12,13 +11,10 @@ function fetchBrowsePage(collection) {
     exclude_free: "false",
     collection,
   });
-  return fetch(`${baseUrl}${query}`, {
-    headers: { "X-API-Key": apiKey },
-  })
+  return fetch(`${baseUrl}${query}`)
     .then((res) => res.json())
     .then((data) => data.data || [])
     .catch((err) => Promise.reject(err));
 }
 
 export default fetchBrowsePage;
-export { apiKey };
